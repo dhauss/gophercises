92bceb483e32fe76ca7e70c0af68140408f7e28e
Dave <davidkhauss@gmail.com>
2024-07-09 18:41:15 +0000

    boltdb basic functionality tested

--- tasks_cli/main.go
+++ tasks_cli/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "task/cmd"
+import (
+	"log"
+	"task/cmd"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
 
 func main() {
+	db, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	cmd.RootCmd.Execute()
 }
